266cfbfc9466a7341ee00669e2e12c1d00b72d79
ArkaprabhaChakraborty <ArkaprabhaChakraborty@users.noreply.github.com>
2023-01-11 16:10:13 +0000

    deploy: 4fe0495447728821605e8638970d49bbcd1951d4

--- js/themetoggle.js
+++ js/themetoggle.js
@@ -2,13 +2,11 @@ function setTheme(mode) {
     localStorage.setItem("theme-storage", mode);
     if (mode === "dark") {
         document.getElementById("darkModeStyle").disabled=false;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"sun\"></i>";
-        feather.replace()
+        document.getElementById("dark-mode-toggle").innerHTML = feather.icon.sun.toSvg({'style':'max-height: 15px !important'});
     } else if (mode === "light") {
         document.getElementById("darkModeStyle").disabled=true;
-        document.getElementById("dark-mode-toggle").innerHTML = "<i data-feather=\"moon\"></i>";
-        feather.replace()
-    }
+        document.getElementById("dark-mode-toggle").innerHTML = feather.icon.moon.toSvg({'style':'max-height: 15px !important'});;
+    }    
 }
 
 function toggleTheme() {
